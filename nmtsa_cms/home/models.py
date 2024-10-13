bf0cda4892f234c6208dd80ef66ec7e9a76ec251
rtwoo <rtwoo@asu.edu>
2024-10-13 07:01:20 -0700

    Return to basic GDrivePage, will use multi-select

--- nmtsa_cms/home/models.py
+++ nmtsa_cms/home/models.py
@@ -53,4 +53,45 @@ class HomePage(Page):
         FieldPanel('body'),
     ]
     
-from wagtail.admin.viewsets.chooser import ChooserViewSet
+class GDrivePage(Page):
+    # add the Hero section of HomePage:
+    hero_text = models.CharField(
+        blank=True,
+        max_length=255, help_text="Write an introduction for the site"
+    )
+    hero_cta = models.CharField(
+        blank=True,
+        verbose_name="Hero CTA",
+        max_length=255,
+        help_text="Text to display on Call to Action",
+    )
+    hero_cta_link = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+        verbose_name="Hero CTA link",
+        help_text="Choose a page to link to for the Call to Action",
+    )
+
+    body = RichTextField(blank=True)
+
+    # modify your content_panels:
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [
+                FieldPanel("hero_text"),
+                FieldPanel("hero_cta"),
+                FieldPanel("hero_cta_link"),
+            ],
+            heading="Hero section",
+        ),
+        FieldPanel('body'),
+    ]
+    
+    def get_context(self, request):
+        context = super().get_context(request)
+        context['GOOGLE_CLIENT_ID'] = settings.SOCIALACCOUNT_PROVIDERS['google']['APP']['client_id']
+        print('context:', context)
+        return context
