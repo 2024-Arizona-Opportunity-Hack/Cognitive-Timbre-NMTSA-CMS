4155cf264c03c7c8030ee29991fb1b213d858e5d
Kiri110K <kirill.a.chernov@gmail.com>
2024-10-12 18:27:11 -0700

    Minor shuffle

--- nmtsa_cms/nmtsa_cms/signals.py
+++ nmtsa_cms/nmtsa_cms/signals.py
@@ -5,33 +5,14 @@ from django.dispatch import receiver
 from django.core.signals import request_finished
 
 test_dict = {
-    
+    'user': 'userfiles',
+    'editor': 'editorfiles',
+    'admin': 'adminfiles'
 }
 
 @receiver(m2m_changed, sender=User.groups.through)
 def role_change_handler(sender, instance, action, pk_set, **kwargs):
-    if action == "post_add":
-        added_groups = Group.objects.filter(pk__in=pk_set)
-        for group in added_groups:
-            print(f'Group added: {group.name}')
-            # Handle added groups here
-            # Example: Make an external API call for added groups
-            # api_url = "https://external-api.example.com/role-change"
-            # payload = {
-            #     "role_name": group.name,
-            #     "action": "added"
-            # }
-            # headers = {
-            #     "Content-Type": "application/json",
-            #     "Authorization": "Bearer YOUR_API_TOKEN"
-            # }
-            # response = requests.post(api_url, json=payload, headers=headers)
-            # if response.status_code == 200:
-            #     print("API call successful for added group")
-            # else:
-            #     print("API call failed for added group")
-
-    elif action == "post_remove":
+    if action == "post_remove":
         removed_groups = Group.objects.filter(pk__in=pk_set)
         for group in removed_groups:
             print(f'Group removed: {group.name}')
@@ -70,3 +51,27 @@ def role_change_handler(sender, instance, action, pk_set, **kwargs):
         #     print("API call successful for clearing all groups")
         # else:
         #     print("API call failed for clearing all groups")
+    
+    elif action == "post_add":
+        added_groups = Group.objects.filter(pk__in=pk_set)
+        for group in added_groups:
+            print(f'Group added: {group.name}')
+
+            
+
+            # Handle added groups here
+            # Example: Make an external API call for added groups
+            # api_url = "https://external-api.example.com/role-change"
+            # payload = {
+            #     "role_name": group.name,
+            #     "action": "added"
+            # }
+            # headers = {
+            #     "Content-Type": "application/json",
+            #     "Authorization": "Bearer YOUR_API_TOKEN"
+            # }
+            # response = requests.post(api_url, json=payload, headers=headers)
+            # if response.status_code == 200:
+            #     print("API call successful for added group")
+            # else:
+            #     print("API call failed for added group")
