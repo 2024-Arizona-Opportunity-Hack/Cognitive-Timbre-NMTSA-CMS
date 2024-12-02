0ce753a3cb5047e97316b232b369c9d7df9fc1e1
code-wolf-byte <hackerstark37@gmail.com>
2024-12-02 12:55:16 -0700

    Copied exisitng code to a new app

--- nmtsa_cms/blog/models.py
+++ nmtsa_cms/blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from wagtail.models import Page, StreamField
+from wagtail.admin.panels import FieldPanel
+from blog.components.models import TextBlock, ImageBlock, ButtonBlock, EmbedBlock, BlogPostsBlock, VideoPostsBlock, FilesBlock, ContentBlock  # Import your custom blocks
 
-# Create your models here.
+
+class Blog(Page):  # Changed from ServicesPage to AddPage
+    intro = models.CharField(max_length=250, blank=True, help_text="Short intro text")
+
+    body = StreamField([
+        ('text', TextBlock()),
+        ('image', ImageBlock()),
+        ('button', ButtonBlock()),
+        ('embed', EmbedBlock()),
+        ('blog_posts', BlogPostsBlock()),
+        ('video_posts', VideoPostsBlock()),
+        ('files', FilesBlock()),
+        ('content', ContentBlock()),  # This is where ContentBlock can be added up to 10 times
+    ], null=True, blank=True, max_num=10)  # Limit to 10 ContentBlock entries
+
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro'),
+        FieldPanel('body'),
+    ]
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        return context
