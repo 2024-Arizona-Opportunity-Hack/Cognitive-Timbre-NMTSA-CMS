fe25b9bb5e51a43baaf186500495d45aec60a7c2
Aaditya Jindal <74290459+RETR0-OS@users.noreply.github.com>
2024-12-03 19:42:01 -0700

    Delete nmtsa_cms directory

--- nmtsa_cms/nmtsa_cms/utils.py
+++
@@ -1,3 +0,0 @@
-from home.models import File
-import gdrive_sharing as gdrive
-
--- nmtsa_cms/nmtsa_cms/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from wagtail.admin import widgets
-
-def manage_files_view(request):
-    # Render a simple page for file management
-    return render(request, 'manage_files.html', {})
